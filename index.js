79c566802d313b764178471b36fdb9045d9ce30c
Chasith-Randima <chasith19@gmail.com>
2023-03-21 17:19:23 +0530

    initialized server and mongoose database

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
+// mongodb connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
