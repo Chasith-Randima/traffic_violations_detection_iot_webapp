52a083cb90934886cde7ac49fa3b69acbb3bc7fe
Chasith-Randima <chasith19@gmail.com>
2023-04-13 18:09:18 +0530

    created case model police model.

--- models/caseModel.js
+++ models/caseModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
+// case schema
 const caseSchema = new mongoose.Schema(
   {
     receivedTime: {
